283365b2ec530361e29313d087682a97e6b0fd5f
Tor Andreas Røsæg <to.an@hotmail.no>
2025-05-31 18:45:10 +0200

    added updateScore function

--- src/main/java/sport/ScoreBoard.java
+++ src/main/java/sport/ScoreBoard.java
@@ -4,7 +4,7 @@ import java.util.Vector;
 import sport.Match;
 
 public class ScoreBoard {
-    Vector<Match> Matches = new Vector<Match>();
+    Vector<Match> matches = new Vector<Match>();
 
     void StartMatch(String home, String away){
         Match game = new Match();
@@ -12,9 +12,12 @@ public class ScoreBoard {
         game.homeTeam = home;
         game.awayScore = 0;
         game.awayTeam = away;
-        Matches.add(game);
+        matches.add(game);
     }
     void UpdateScore(int home, int away, int match){
+        Match updatingMatch = matches.get(match);
+        updatingMatch.homeScore = home;
+        updatingMatch.awayScore = away;
     }
     void FinishMatch(int match){}
     void Summary(){
